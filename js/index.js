d7f85c6a064526cd126833c76b83e165ac2655ac
Ruchita <ruchita.s@soltelabs.com>
2023-11-22 16:41:18 +0530

    bug fixing

--- js/index.js
+++ js/index.js
@@ -1,8 +1,44 @@
+// function toggleText() {
+//   var talkText = document.getElementById("chat-msg");
+//   console.log(talkText);
+//   if (talkText.innerHTML === "Welcome to FaceX...") {
+//     talkText.innerHTML =
+//       "We create the most <br> trusted facial aesthetic <br> brands.";
+//   } else {
+//     talkText.innerHTML = "Welcome to FaceX...";
+//   }
+// }
+const texts = [
+  "We create the most trusted facial aesthetic brands. ",
+  "Welcome to FaceX...",
+];
+let currentTextIndex = 0;
+let isTyping = false;
+
 function toggleText() {
-  var talkText = document.getElementById("chat-msg");
-  if (talkText.innerHTML === "Welcome to FaceX...") {
-    talkText.innerHTML = "We create the most trusted facial aesthetic brands.";
+  const typewriterText = document.getElementById("typewriter-text");
+
+  if (isTyping) {
+    isTyping = false;
   } else {
-    talkText.innerHTML = "Welcome to FaceX...";
+    typeWriter(typewriterText, texts[currentTextIndex]);
+    currentTextIndex = (currentTextIndex + 1) % texts.length;
   }
+}
+
+function typeWriter(element, text) {
+  isTyping = true;
+  let charIndex = 0;
+
+  function type() {
+    if (charIndex < text.length) {
+      element.innerHTML = text.substring(0, charIndex + 1);
+      charIndex++;
+      setTimeout(type, 100);
+    } else {
+      isTyping = false;
+    }
+  }
+
+  type();
 }
